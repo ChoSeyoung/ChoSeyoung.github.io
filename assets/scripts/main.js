97e20ce725b352472629288af019460b92b4e670
ChoSeyoung <a01075511157@gmail.com>
2024-08-06 23:29:13 +0900

    프로젝트 내용 추가

---
+++ assets/scripts/main.js
@@ -0,0 +1,29 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var form = event.target;
+    var formData = new FormData(form);
+
+    fetch(form.action, {
+        method: form.method,
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(function(response) {
+        if (response.ok) {
+            alert('Thank you for your message!');
+            form.reset();
+        } else {
+            response.json().then(function(data) {
+                if (Object.hasOwn(data, 'errors')) {
+                    alert(data["errors"].map(error => error["message"]).join(", "));
+                } else {
+                    alert('Oops! There was a problem submitting your form');
+                }
+            });
+        }
+    }).catch(function(error) {
+        alert('Oops! There was a problem submitting your form');
+    });
+});
